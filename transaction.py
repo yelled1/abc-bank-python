9986f54f83a1c3dfcc79313ae14ba68e25d0d83e
yelled1 <mailhyoon@gmail.com>
2016-08-22 21:37:56 -0400

    Delete transaction.py

--- transaction.py
+++
@@ -1,7 +0,0 @@
-import datetime as D
-
-class Transaction:
-    def __init__(self, amount, tDate=D.date.today(), tType=0):
-        self.amount = amount
-        self.transactionDate = tDate
-        self.tType = tType
